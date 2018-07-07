c36274485970e0f988c21be08f1ffe0006834007
7erra <38866692+7erra@users.noreply.github.com>
2018-07-07 18:24:04 +0200

    commit 1
    Start of the repo

---
+++ @Terra's Editing Extensions/mod.cpp
@@ -0,0 +1,7 @@
+name = "7erra's Editing Extensions";
+picture = "TER_editing_data\icons\ingame_thumb.paa";
+logoSmall = "TER_editing_data\icons\logo_small.paa";
+logo = "TER_editing_data\icons\logo.paa";
+overview = "By adding more options to the escape menu's debug console this mod aims to improve the editing experience while creating no dependencies. Also adds smaller features to the rest of the game. For a full list take a look at the Steam Workshop page.";
+action = "https://steamcommunity.com/sharedfiles/filedetails/?id=1387934106";
+tooltipOwned = "Editing improved";
