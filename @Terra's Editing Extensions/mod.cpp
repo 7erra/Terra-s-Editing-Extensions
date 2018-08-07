7d0ee4ec8358bc053f1443c42c1f9851ddcbda94
7erra <38866692+7erra@users.noreply.github.com>
2018-08-07 03:02:33 +0200

    v1.7.1
    New logo and thumbnail

--- @Terra's Editing Extensions/mod.cpp
+++ @Terra's Editing Extensions/mod.cpp
@@ -1,6 +1,6 @@
 name = "7erra's Editing Extensions";
-picture = "TER_editing\icons\ingame_thumb.paa";
-logoSmall = "TER_editing\icons\logo_small.paa";
+picture = "TER_editing\icons\2048_thumb_V2.paa";
+logoSmall = "TER_editing\icons\logo.paa";
 logo = "TER_editing\icons\logo.paa";
 overview = "By adding more options to the escape menu's debug console this mod aims to improve the editing experience while creating no dependencies. Also adds smaller features to the rest of the game. For a full list take a look at the Steam Workshop page.";
 action = "https://steamcommunity.com/sharedfiles/filedetails/?id=1387934106";
