979ef13c9a4ebd1a58ce90da57ec09c7ab65046a
7erra <38866692+7erra@users.noreply.github.com>
2020-09-07 00:22:19 +0200

    changed paths to more versatile settings

--- .vscode/launch.py
+++ .vscode/launch.py
@@ -1,6 +1,7 @@
 #%% Libraries and...
 from pathlib import Path
 import subprocess
+import json
 # ... variables
 a3_version = 'arma3diag_x64'
 a3_dir = Path('M:\SteamLibraryM\steamapps\common\Arma 3')
@@ -8,10 +9,17 @@ a3_exe = a3_dir / f'{a3_version}.exe'
 
 modslist = [r'!Workshop\@CBA_A3',
             r'!Workshop\@Pythia',
-            r"P:\-Terra-s-Editing-Extensions\@Terra's Editing Extensions"]
+            r"P:\Terra-s-Editing-Extensions\@Terra's Editing Extensions"]
 mods = f'-mod={";".join(modslist)}'
 
-mission = '"E:\Documents\Arma 3\missions\TER_Editing_mission.vr\mission.sqm"'
+with open('TER_Editing.code-workspace', 'r') as f:
+    workspace_settings = json.load(f)
+try:
+    mission_root = workspace_settings['settings']['A3MissionRoot']
+    mission = f'"{mission_root}\\TER_Editing_mission.vr\\mission.sqm"'
+except KeyError:
+    print('Mission to load is not set in the TER_Editing.code-workspace settings!')
+    mission = ''
 #%% Launch the game with parameters
 print(f'Launching {a3_exe}')
 subprocess.Popen([a3_exe.absolute(),
