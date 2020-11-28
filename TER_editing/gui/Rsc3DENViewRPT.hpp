f6db77544cf91af201e2a1798b9c03cc478cbcf4
R3voA3 <robertseibel@outlook.de>
2020-11-28 03:02:37 +0100

    Update Rsc3DENViewRPT.hpp
    - GUI is using CUI grid macros now

--- TER_editing/gui/Rsc3DENViewRPT.hpp
+++ TER_editing/gui/Rsc3DENViewRPT.hpp
@@ -1,98 +1,98 @@
+//Width and height of GUI. Can be used to adjust size efficiently
+#define DIALOG_W 35
+#define DIALOG_H 20
+
 class TER_3DENViewRPT //To-Do: Use macro system for coordinates at some point
 {
 	idd = -1;
 	movingEnable = "true";
 	onLoad = "[_this # 0, 'onLoad'] execVM ""TER_Editing\gui\scripts\Rsc3DENViewRPT.sqf"";";
 	class ControlsBackground
-	{
-		class BackgroundDisable: ctrlStaticBackgroundDisable
-		{
-		};
-		class BackgroundDisableTiles: ctrlStaticBackgroundDisableTiles
-		{
-		};
-		class Background: ctrlStaticBackground
-		{
-			x = "(	((getResolution select 2) * 0.5 * pixelW) - ((200) / 2 * (pixelW * pixelGrid * 	0.50)))";
-			y = "(safezoneY + 16 * (pixelH * pixelGrid * 	0.50)) + (5 * (pixelH * pixelGrid * 	0.50))";
-			w = "200 * (pixelW * pixelGrid * 	0.50)";
-			h = "106 * (pixelH * pixelGrid * 	0.50)";
-		};
-		class TitleHeader: ctrlStaticTitle
-		{
-			text = "Latest RPT";
-			x = "(	((getResolution select 2) * 0.5 * pixelW) - ((200) / 2 * (pixelW * pixelGrid * 	0.50)))";
-			y = "(safezoneY + 16 * (pixelH * pixelGrid * 	0.50))";
-			w = "200 * (pixelW * pixelGrid * 	0.50)";
-			h = "(5 * (pixelH * pixelGrid * 	0.50))";
-		};
-	};
-	class Controls
-	{
-		class Group: ctrlControlsGroup
-		{
-			x = "(	((getResolution select 2) * 0.5 * pixelW) - ((200) / 2 * (pixelW * pixelGrid * 	0.50)))";
-			y = "(safezoneY + 16 * (pixelH * pixelGrid * 	0.50)) + (5 * (pixelH * pixelGrid * 	0.50))";
-			w = "200 * (pixelW * pixelGrid * 	0.50)";
-			h = "(106 - 7) * (pixelH * pixelGrid * 	0.50)";
-			class Controls
-			{
-				class Content: ctrlEditMulti
-				{
-					idc = 100;
-					x = 0;
-					y = 0;
-					w = "40 * (pixelW * pixelGrid * 	0.50)";
-					h = "(106 - 7) * (pixelH * pixelGrid * 	0.50)";
-					style = "0x200 + 0x10";
-					canModify = "false";
-				};
-			};
-		};
-		class Lines: ctrlStatic
-		{
-			text = "Lines:";
-			tooltip = "Define the number of lines to be shown. 50 means the last 50 lines are shown. Use a small number to speed up the process. Use a very large number to display all lines.";
-			x = "(	((getResolution select 2) * 0.5 * pixelW) - ((200) / 2 * (pixelW * pixelGrid * 	0.50))) + (200 - 80) * (pixelW * pixelGrid * 	0.50)";
-			y = "(safezoneY + 16 * (pixelH * pixelGrid * 	0.50)) + 105 * (pixelH * pixelGrid * 	0.50)";
-			w = "15 * (pixelW * pixelGrid * 	0.50)";
-			h = "(5 * (pixelH * pixelGrid * 	0.50))";
-		};
-		class EditLines: ctrlEdit
-		{
-			idc = 200;
-			tooltip = "Define the number of lines to be shown. 50 means the last 50 lines are shown. Use a small number to speed up the process. Use a very large number to display all lines.";
-			x = "(	((getResolution select 2) * 0.5 * pixelW) - ((200) / 2 * (pixelW * pixelGrid * 	0.50))) + (200 - 65) * (pixelW * pixelGrid * 	0.50)";
-			y = "(safezoneY + 16 * (pixelH * pixelGrid * 	0.50)) + 105 * (pixelH * pixelGrid * 	0.50)";
-			w = "12 * (pixelW * pixelGrid * 	0.50)";
-			h = "(5 * (pixelH * pixelGrid * 	0.50))";
-			maxChars = 3;
-		};
-		class Progress: ctrlProgress
-		{
-			idc = 300;
-			tooltip = "Shows the progress. A full bar indicates that all available lines were loaded.";
-			x = "(	((getResolution select 2) * 0.5 * pixelW) - ((200) / 2 * (pixelW * pixelGrid * 	0.50)))";
-			y = "(safezoneY + 16 * (pixelH * pixelGrid * 	0.50)) + 104 * (pixelH * pixelGrid * 	0.50)";
-			w = "200 * (pixelW * pixelGrid * 	0.50)";
-			h = "(pixelH * pixelGrid * 	0.50)";
-			colorFrame[] = {0,0,0,0};
-		};
-		class Reload: ctrlButton
-		{
-			text = "Reload";
-			x = "(	((getResolution select 2) * 0.5 * pixelW) - ((200) / 2 * (pixelW * pixelGrid * 	0.50))) + (174 - 26) * (pixelW * pixelGrid * 	0.50)";
-			y = "(safezoneY + 16 * (pixelH * pixelGrid * 	0.50)) + 105 * (pixelH * pixelGrid * 	0.50)";
-			w = "25 * (pixelW * pixelGrid * 	0.50)";
-			h = "(5 * (pixelH * pixelGrid * 	0.50))";
-			onButtonClick = "[_this # 0, 'reload'] execVM ""TER_Editing\gui\scripts\Rsc3DENViewRPT.sqf"";";
-		};
-		class Close: ctrlButtonClose
-		{
-			x = "(	((getResolution select 2) * 0.5 * pixelW) - ((200) / 2 * (pixelW * pixelGrid * 	0.50))) + 174 * (pixelW * pixelGrid * 	0.50)";
-			y = "(safezoneY + 16 * (pixelH * pixelGrid * 	0.50)) + 105 * (pixelH * pixelGrid * 	0.50)";
-			w = "25 * (pixelW * pixelGrid * 	0.50)";
-			h = "(5 * (pixelH * pixelGrid * 	0.50))";
-		};
-	};
+  {
+    class BackgroundDisable: ctrlStaticBackgroundDisable {};
+    class BackgroundDisableTiles: ctrlStaticBackgroundDisableTiles {};
+    class Background: ctrlStaticBackground
+    {
+      x = CUI_GRID_X - DIALOG_W / 2 * CUI_GRID_W;
+      y = CUI_GRID_Y - 13 * CUI_GRID_H;
+      w = DIALOG_W * CUI_GRID_W;
+      h = DIALOG_H * CUI_GRID_H;
+    };
+    class TitleHeader: ctrlStaticTitle
+    {
+      text = "Latest RPT";
+      x = CUI_GRID_X - DIALOG_W / 2 * CUI_GRID_W;
+      y = CUI_GRID_Y - 14 * CUI_GRID_H;
+      w = DIALOG_W * CUI_GRID_W;
+      h = CUI_GRID_H;
+    };
+  };
+  class Controls
+  {
+    class Group: ctrlControlsGroup
+    {
+      x = CUI_GRID_X - DIALOG_W / 2 * CUI_GRID_W;
+      y = CUI_GRID_Y - 13 * CUI_GRID_H;
+      w = DIALOG_W * CUI_GRID_W;
+      h = DIALOG_H * CUI_GRID_H - CUI_GRID_H - 3/5 * CUI_GRID_H;
+      class Controls
+      {
+        class Content: ctrlEditMulti
+        {
+          idc = IDC_RPT_CONTENT;
+          x = 0;
+          y = 0;
+          w = DIALOG_W * CUI_GRID_W;
+          h = DIALOG_H * CUI_GRID_H - CUI_GRID_H - 3/5 * CUI_GRID_H;
+          style = ST_NO_RECT + ST_MULTI;
+          canModify = false;
+        };
+      };
+    };
+    class Lines: ctrlStatic
+    {
+      text = "Lines:";
+      tooltip = "Define the number of lines to be shown. 30 means the last 30 lines are shown. Use a small number to speed up the process. Use a very large number to display all lines.";
+      x = CUI_GRID_X + DIALOG_W / 2 * CUI_GRID_W - 12 * CUI_GRID_W;
+      y = CUI_GRID_Y + DIALOG_H * CUI_GRID_H - 14.2 * CUI_GRID_H;
+      w = 3 * CUI_GRID_W;
+      h = CUI_GRID_H;
+    };
+    class EditLines: ctrlEdit
+    {
+      idc = IDC_RPT_LINES;
+      tooltip = "Define the number of lines to be shown. 30 means the last 30 lines are shown. Use a small number to speed up the process. Use a very large number to display all lines.";
+      x = CUI_GRID_X + DIALOG_W / 2 * CUI_GRID_W - 9 * CUI_GRID_W;
+      y = CUI_GRID_Y + DIALOG_H * CUI_GRID_H - 14.2 * CUI_GRID_H;
+      w = 3 * CUI_GRID_W;
+      h = CUI_GRID_H;
+      maxChars = 3;
+    };
+    class Progress: ctrlProgress
+    {
+      idc = IDC_RPT_PROGRESS;
+      tooltip = "Shows the progress. A full bar indicates that all available lines were loaded.";
+      x = CUI_GRID_X - DIALOG_W / 2 * CUI_GRID_W;
+      y = CUI_GRID_Y + DIALOG_H * CUI_GRID_H - 14.6 * CUI_GRID_H;
+      w = DIALOG_W * CUI_GRID_W;
+      h = 1/5 * CUI_GRID_H;
+      colorFrame[] = {0,1,1,1};
+    };
+    class Reload: ctrlButton
+    {
+      text = "Reload";
+      x = CENTERED_X(DIALOG_W) + (174 - 26) * GRID_W;
+      y = DIALOG_TOP + 105 * UI_GRID_H;
+      w = 25 * GRID_W;
+      h = CUI_GRID_H;
+      onButtonClick = "[_this # 0, 'reload'] call ENH_fnc_rpt_load";
+    };
+    class Close: ctrlButtonClose
+    {
+      x = CUI_GRID_X + DIALOG_W / 2 * CUI_GRID_W - 5.2 * CUI_GRID_W;
+      y = CUI_GRID_Y + DIALOG_H * CUI_GRID_H - 14.2 * CUI_GRID_H;
+      w = 5 * CUI_GRID_W;
+      h = CUI_GRID_H;
+    };
+  };
 };
