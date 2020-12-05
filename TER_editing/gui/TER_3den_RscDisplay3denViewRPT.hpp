48ba93e2f5ed08828862065f4547986053f8f77c
7erra <38866692+7erra@users.noreply.github.com>
2020-12-05 01:40:55 +0100

    latest3denrpt: full height of screen

--- TER_editing/gui/TER_3den_RscDisplay3denViewRPT.hpp
+++ TER_editing/gui/TER_3den_RscDisplay3denViewRPT.hpp
@@ -1,5 +1,5 @@
 #define DIALOG_W 35
-#define DIALOG_H 20
+#define DIALOG_H (safeZoneH - 2 * CUI_GRID_H)
 class TER_3den_RscDisplay3denViewRPT
 {
 	idd = -1;
@@ -13,30 +13,31 @@ class TER_3den_RscDisplay3denViewRPT
 		class BackgroundDisableTiles: ctrlStaticBackgroundDisableTiles
 		{
 		};
-		class Background: ctrlStaticBackground
-		{
-			x = CUI_GRID_X - DIALOG_W / 2 * CUI_GRID_W;
-			y = CUI_GRID_Y - 13 * CUI_GRID_H;
-			w = DIALOG_W * CUI_GRID_W;
-			h = DIALOG_H * CUI_GRID_H;
-		};
 		class TitleHeader: ctrlStaticTitle
 		{
 			text = "Latest RPT";
 			x = CUI_GRID_X - DIALOG_W / 2 * CUI_GRID_W;
-			y = CUI_GRID_Y - 14 * CUI_GRID_H;
+			y = safeZoneY + 1 * UI_GRID_H;
 			w = DIALOG_W * CUI_GRID_W;
 			h = CUI_GRID_H;
+		};
+		class Background: ctrlStaticBackground
+		{
+			x = CUI_GRID_X - DIALOG_W / 2 * CUI_GRID_W;
+			y = safeZoneY + 2 * UI_GRID_H;
+			w = DIALOG_W * CUI_GRID_W;
+			h = DIALOG_H - 1 * CUI_GRID_H;
 		};
 	};
 	class Controls
 	{
 		class ContentGroup: ctrlControlsGroup
 		{
-			x = CUI_GRID_X - DIALOG_W / 2 * CUI_GRID_W;
-			y = CUI_GRID_Y - 13 * CUI_GRID_H;
-			w = DIALOG_W * CUI_GRID_W;
-			h = DIALOG_H * CUI_GRID_H - CUI_GRID_H - 3/5 * CUI_GRID_H;
+			x = CUI_GRID_X - 0.5 * DIALOG_W * CUI_GRID_W + 0.1 * CUI_GRID_W;
+			y = safeZoneY + 2.1 * CUI_GRID_H;
+			w = (DIALOG_W - 0.2) * CUI_GRID_W;
+			h = DIALOG_H - 2.2 * CUI_GRID_H;
+			colorBackground[] = {1,0,0,0.1};
 			class Controls
 			{
 				class Content: ctrlStructuredText
@@ -58,55 +59,25 @@ class TER_3den_RscDisplay3denViewRPT
 				};
 			};
 		};
-		class Lines: ctrlStatic
-		{
-			text = "Lines:";
-			tooltip = "Define the number of lines to be shown. 30 means the last 30 lines are shown. Use a small number to speed up the process. Use a very large number to display all lines.";
-			x = CUI_GRID_X + DIALOG_W / 2 * CUI_GRID_W - 12 * CUI_GRID_W;
-			y = CUI_GRID_Y + DIALOG_H * CUI_GRID_H - 14.2 * CUI_GRID_H;
-			w = 3 * CUI_GRID_W;
-			h = CUI_GRID_H;
-		};
-		class EditLines: ctrlEdit
-		{
-			idc = IDC_RSCDISPLAY3DENLASTRPT_EDITLINES;
-			tooltip = "Define the number of lines to be shown. 30 means the last 30 lines are shown. Use a small number to speed up the process. Use a very large number to display all lines.";
-			x = CUI_GRID_X + DIALOG_W / 2 * CUI_GRID_W - 9 * CUI_GRID_W;
-			y = CUI_GRID_Y + DIALOG_H * CUI_GRID_H - 14.2 * CUI_GRID_H;
-			w = 3 * CUI_GRID_W;
-			h = CUI_GRID_H;
-			maxChars = 3;
-		};
-		class Progress: ctrlProgress
-		{
-			idc = IDC_RSCDISPLAY3DENLASTRPT_PROGRESS;
-			tooltip = "Shows the progress. A full bar indicates that all available lines were loaded.";
-			x = CUI_GRID_X - DIALOG_W / 2 * CUI_GRID_W;
-			y = CUI_GRID_Y + DIALOG_H * CUI_GRID_H - 14.6 * CUI_GRID_H;
-			w = DIALOG_W * CUI_GRID_W;
-			h = 1/5 * CUI_GRID_H;
-			colorFrame[] = {0,0,0,0};
-		};
 		class Reload: ctrlButton
 		{
 			idc = IDC_RSCDISPLAY3DENLASTRPT_RELOAD;
 			text = "Reload";
-			x = CUI_GRID_X - DIALOG_W / 2 * CUI_GRID_W + 0.1 * CUI_GRID_W;
-			y = CUI_GRID_Y + DIALOG_H * CUI_GRID_H - 14.2 * CUI_GRID_H;
+			x = CUI_GRID_X - 0.5 * DIALOG_W * CUI_GRID_W + 0.1 * CUI_GRID_W;
+			y = safeZoneY + DIALOG_H - 0.1 * CUI_GRID_H;
 			w = 5 * UI_GRID_W;
 			h = CUI_GRID_H;
-			//onButtonClick = "[_this # 0, 'reload'] execVM ""TER_Editing\gui\scripts\Rsc3DENViewRPT.sqf"";";
 		};
 		class Open: Reload
 		{
-			idc = -1;
+			idc = IDC_RSCDISPLAY3DENLASTRPT_OPEN;
 			text = "Open RPT";
 			x = CUI_GRID_X - DIALOG_W / 2 * CUI_GRID_W + 5.2 * CUI_GRID_W;
 		};
 		class Close: ctrlButtonClose
 		{
-			x = CUI_GRID_X + DIALOG_W / 2 * CUI_GRID_W - 5.2 * CUI_GRID_W;
-			y = CUI_GRID_Y + DIALOG_H * CUI_GRID_H - 14.2 * CUI_GRID_H;
+			x = CUI_GRID_X + DIALOG_W / 2 * CUI_GRID_W - 5.1 * CUI_GRID_W;
+			y = safeZoneY + DIALOG_H - 0.1 * CUI_GRID_H;
 			w = 5 * CUI_GRID_W;
 			h = CUI_GRID_H;
 		};
